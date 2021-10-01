654c9e019057ebc78013115b8eb29a9544b4c9a0
kiliczsh <muhammeddkilicc@gmail.com>
2021-10-02 00:37:57 +0300

    Resolves #1, test directory fixed.

--- services/git.go
+++ services/git.go
@@ -1,11 +1,13 @@
 package services
 
 import (
+	"fmt"
 	"github.com/kiliczsh/gondit/model"
 	"gopkg.in/src-d/go-git.v4"
 	"io/ioutil"
 	"log"
 	"os"
+	"os/exec"
 )
 
 func Clone(scan model.Scan)  string {
@@ -25,5 +27,12 @@ func Clone(scan model.Scan)  string {
 		log.Fatal(err)
 		return ""
 	}
-	return dir
+
+	cmd := exec.Command("cp", "-r", dir, "/tmp/src")
+	_, e := cmd.Output()
+	if e != nil && e.Error() != "exit status 1" {
+		fmt.Println(e)
+		return ""
+	}
+	return "/tmp/src"
 }
