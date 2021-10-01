618b26f379f019094a11e42bc43fe221a0b32f69
kiliczsh <muhammeddkilicc@gmail.com>
2021-10-02 00:19:03 +0300

    Improved test execution for async clone.

--- services/bash.go
+++ services/bash.go
@@ -7,16 +7,22 @@ import (
 	"os/exec"
 )
 
-func Execute(db *gorm.DB, scan model.Scan) string {
+func Execute(db *gorm.DB, scan model.Scan) string{
 
-	cmd := exec.Command("docker", "run", "--rm", "-v", "/tmp/src:/code",
-		"opensorcery/bandit", "-f", "json", "-r", "/code")
+	res := Clone(scan)
+	if res == "" {
+		return fmt.Sprintf("Cloning %s failed!", scan.Url)
+	}
+
+	cmd := exec.Command("docker", "run", "--rm", "-v", res + ":/code", "opensorcery/bandit", "-f", "json", "-r", "/code")
 	stdout, err := cmd.Output()
-	if err != nil {
+	if err != nil && err.Error() != "exit status 1" {
 		fmt.Println(err.Error())
+		return err.Error()
 	}
 	scan.Response = string(stdout)
 	db.Save(&scan)
 	db.Find(&scan, scan.ID)
+
 	return scan.Response
 }
